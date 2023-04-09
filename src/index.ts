06b4bfd8b6dabd5f3fbfd79dea7db24bf21bdcb4
JosephJasin <Josef20606@gmail.com>
2023-04-09 08:56:44 +0300

    SSL part 2

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import {prayerTimesRouter} from './routes/prayerTimes';
 import * as https from "https";
 import * as fs from "fs";
+import * as http from "http";
 
 dotenv.config();
 
@@ -17,6 +18,10 @@ app.get('/', (req: Request, res: Response) => {
 
 app.use('/v1', prayerTimesRouter)
 
+http.createServer(app).listen(80, () => {
+    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+})
+
 https.createServer(
     {
         key: fs.readFileSync('/etc/letsencrypt/path/to/key.pem'),
@@ -25,6 +30,6 @@ https.createServer(
     },
     app
 ).listen(port, () => {
-        console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+        console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
     }
 )
