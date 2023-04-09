0b8acd35772bfc3c2a4c09e30bc70e3d6eb53d22
JosephJasin <Josef20606@gmail.com>
2023-04-09 09:26:05 +0300

    SSL Part 3

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,15 @@ import {prayerTimesRouter} from './routes/prayerTimes';
 import * as https from "https";
 import * as fs from "fs";
 import * as http from "http";
+import path from "path";
 
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT;
+const httpsPort = process.env.HTTPS_PORT;
+const httpPort = process.env.HTTP_PORT;
 
-app.use(express.static(__dirname + '/static', {dotfiles: 'allow'}))
+app.use(express.static(path.join(process.cwd() , 'static'), {dotfiles: 'allow'}))
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Express + TypeScript Server');
@@ -19,17 +21,17 @@ app.get('/', (req: Request, res: Response) => {
 app.use('/v1', prayerTimesRouter)
 
 http.createServer(app).listen(80, () => {
-    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+    console.log(`⚡️[server]: Server is running at http://localhost:${httpPort}`);
 })
 
-https.createServer(
-    {
-        key: fs.readFileSync('/etc/letsencrypt/path/to/key.pem'),
-        cert: fs.readFileSync('/etc/letsencrypt/path/to/cert.pem'),
-        ca: fs.readFileSync('/etc/letsencrypt/path/to/chain.pem'),
-    },
-    app
-).listen(port, () => {
-        console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
-    }
-)
+// https.createServer(
+//     {
+//         key: fs.readFileSync('/etc/letsencrypt/path/to/key.pem'),
+//         cert: fs.readFileSync('/etc/letsencrypt/path/to/cert.pem'),
+//         ca: fs.readFileSync('/etc/letsencrypt/path/to/chain.pem'),
+//     },
+//     app
+// ).listen(port, () => {
+//         console.log(`⚡️[server]: Server is running at https://localhost:${httpsPort}`);
+//     }
+// )
