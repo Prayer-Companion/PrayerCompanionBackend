f9c774b45b79c37e3aaa9dc8b2e18cd9aa816a30
JosephJasin <Josef20606@gmail.com>
2023-04-09 20:43:29 +0300

    Revert changes

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,11 @@
 import express, {Express, Request, Response} from 'express';
 import dotenv from 'dotenv';
 import {prayerTimesRouter} from './routes/prayerTimes';
-import * as https from "https";
-import * as fs from "fs";
-import * as http from "http";
-import path from "path";
 
 dotenv.config();
 
-const rootPath = path.join(__dirname, '..')
-
 const app: Express = express();
-const httpsPort = process.env.HTTPS_PORT;
-const httpPort = process.env.HTTP_PORT;
-
-app.use(express.static(path.join(rootPath, 'static'), {dotfiles: 'allow'}))
-
-console.log(path.join(rootPath, 'static'))
+const port = process.env.PORT;
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Express + TypeScript Server');
@@ -24,18 +13,6 @@ app.get('/', (req: Request, res: Response) => {
 
 app.use('/v1', prayerTimesRouter)
 
-http.createServer(app).listen(80, () => {
-    console.log(`⚡️[server]: Server is running at http://localhost:${httpPort}`);
+app.listen(port, () => {
+    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 })
-
-// https.createServer(
-//     {
-//         key: fs.readFileSync(path.join(rootPath, '/etc/letsencrypt/path/to/key.pem')),
-//         cert: fs.readFileSync(path.join(rootPath, '/etc/letsencrypt/path/to/cert.pem')),
-//         ca: fs.readFileSync(path.join(rootPath, '/etc/letsencrypt/path/to/chain.pem')),
-//     },
-//     app
-// ).listen(httpsPort, () => {
-//         console.log(`⚡️[server]: Server is running at https://localhost:${httpsPort}`);
-//     }
-// )
